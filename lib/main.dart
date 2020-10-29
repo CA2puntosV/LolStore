edf3c9a1ce66a97ed8ac394cf3d4d2b174f8a148
CA2puntosV <crisitan2puntosv@gmail.comm>
2020-10-29 14:15:36 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:tienda_lol/screens/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/login': (context) => BackApp(),
+      },
+      initialRoute: '/login',
+    );
+  }
+}
