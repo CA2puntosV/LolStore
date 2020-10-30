e0779ea498a0b82c4c1bd27859f70cb848c52b8b
CA2puntosV <crisitan2puntosv@gmail.comm>
2020-10-29 19:07:24 -0500

    Figura del login corregida

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/login': (context) => BackApp(),
+        '/': (context) => LoginPage(),
       },
-      initialRoute: '/login',
+      initialRoute: '/',
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -24,78 +24,26 @@ class LoginPage extends StatelessWidget {
       ),
     );
 
-    final orangeBox = Transform.rotate(
-      angle: -pi / 1.0,
+    final figLog = Transform.rotate(
+      angle: pi / 1.0,
       child: Container(
-        height: 360.0,
-        width: 400,
+        height: 390,
+        width: 300,
         decoration: BoxDecoration(
-            color: Color.fromRGBO(251, 85, 23, 1),
-            borderRadius: BorderRadius.only(
-                topRight: Radius.circular(120.0),
-                topLeft: Radius.circular(20.0))),
+          color: Color(0xFF38688c),
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(250.0),
+            topLeft: Radius.circular(250.0),
+          ),
+        ),
       ),
     );
 
     return Stack(
-      children: <Widget>[gradiente, Positioned(top: -100.0, child: orangeBox)],
+      children: <Widget>[
+        gradiente,
+        Positioned(top: -90.0, left: 50.0, right: 50.0, child: figLog)
+      ],
     );
-  }
-}
-
-class TopScreen extends StatelessWidget {
-  const TopScreen({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(251, 85, 23, 1),
-      ),
-      body: LoginPage(),
-    );
-  }
-}
-
-class Tittles extends StatelessWidget {
-  const Tittles({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(60.0),
-        child: Column(
-          children: <Widget>[image(), desc()],
-        ));
-  }
-
-  Widget image() {
-    return Image(
-      image: AssetImage('assets/logoW.png'),
-      height: 120,
-      width: 120,
-    );
-  }
-
-  Widget desc() {
-    return Container(
-        padding: EdgeInsets.all(40.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              "Registrate",
-              style: TextStyle(
-                  decoration: TextDecoration.none,
-                  color: Colors.white,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold),
-            )
-          ],
-        ));
   }
 }
